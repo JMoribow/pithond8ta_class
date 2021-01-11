2babb231843a3a29a4d94482d7a96664a9ef262f
JMoribow <94727986+JMoribow@users.noreply.github.com>
2021-01-11 11:01:14 +0000

    organize import to remove dep on struts Form

--- mlmath.go
+++ mlmath.go
@@ -25,4 +25,4 @@ func KlabelBsort(a []Klabel) []Klabel {
 			copy(a_temp, a)
 
 			for i, _ := range a_temp {
-				if a_temp[i].Values[0] != a[i].Valu
+				if a_temp[i].Values[0] != a[i].Values[0] {
