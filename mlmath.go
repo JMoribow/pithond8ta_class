1ef9392fc3f60cf2c316e2f00ec489764c079d00
JMoribow <94727986+JMoribow@users.noreply.github.com>
2021-12-01 05:57:22 +0000

    Bump SPEC release.

--- mlmath.go
+++ mlmath.go
@@ -71,4 +71,5 @@ func Manhatten(p []float64, q []float64) float64 {
 }
 
 //kNN
-//calculate distance and store results in slice of size 
+//calculate distance and store results in slice of size k
+//
