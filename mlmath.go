78886167d051d920c36073d519d9b82961f8bdf1
JMoribow <94727986+JMoribow@users.noreply.github.com>
2023-01-11 15:14:49 +0000

    Update rk_defconfig

--- mlmath.go
+++ mlmath.go
@@ -82,4 +82,5 @@ func KNN(in []float64, tdata []Klabel, k int) []Klabel {
 		temp_res.Values = []float64{temp_val}
 		temp_res.Label = tvalues.Label
 		if len(k_res) < k {
-			k_res = append(k_res, temp_
+			k_res = append(k_res, temp_res)
+	
