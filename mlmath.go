5f0cdbfafbea0f5a535c49db89a86b31e56ca14b
JMoribow <94727986+JMoribow@users.noreply.github.com>
2023-03-22 08:41:39 +0000

    Remove debug logs

--- mlmath.go
+++ mlmath.go
@@ -86,4 +86,5 @@ func KNN(in []float64, tdata []Klabel, k int) []Klabel {
 		} else {
 			k_res := KlabelBsort(k_res)
 			if temp_res.Values[0] < k_res[2].Values[0] {
-				k_res[2] = temp_r
+				k_res[2] = temp_res
+			}
